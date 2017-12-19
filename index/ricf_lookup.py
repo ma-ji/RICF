649cebcb200967318b527cfb554ea25fa3b9a88e
Ji Ma <ma47@iu.edu>
2017-12-19 17:26:28 -0500

    update name index

--- index/ricf_lookup.py
+++ index/ricf_lookup.py
@@ -9,18 +9,18 @@ name_index_file=pd.read_csv('https://github.com/ma-ji/RICF/raw/master/index/name
 def ricf_nm2id(fund_nm):
     re_id_set=set(name_index_file[name_index_file.ba_cn==fund_nm]['ricf_oid'])
     if len(re_id_set)==1:
-        return {fund_nm:list(re_id_set)[0]}
+        return list(re_id_set)[0]
     elif len(re_id_set)>1:
-        return {fund_nm:list(re_id_set)}
+        return list(re_id_set)
     elif len(re_id_set)<1:
         return 'NA'
 
 def ricf_id2nm(oid):
     re_nm_set=set(name_index_file[name_index_file.ricf_oid==int(oid)]['ba_cn'])
     if len(re_nm_set)==1:
-        return {oid:list(re_nm_set)[0]}
+        return list(re_nm_set)[0]
     elif len(re_nm_set)>1:
-        return {oid:list(re_nm_set)}
+        return list(re_nm_set)
     elif len(re_nm_set)<1:
         return 'NA'
 
